bb064f12e4a1d0bd4fd9718663b6c4a30174ef14
Pavan.Kumar <pnttiwari99@gmail.com>
2024-04-23 23:03:58 +0530

    header

---
+++ Components/SearchBar.tsx
@@ -0,0 +1,15 @@
+"use Client";
+import SearchManufacturer from "./SearchManufacturer";
+
+const SearchBar = () => {
+  const handleSubmit = () => {};
+  return (
+    <form className="searchbar" onSubmit={handleSubmit}>
+      <div className="searchbar__item">
+        <SearchManufacturer />
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
